695a5fd50978e9f8f20744b7e8597e91008de3dd
Nelson <punkpunkada@gmail.com>
2024-01-27 08:19:48 -0300

    starting project

---
+++ src/main/java/nelsonssoares/ecomshoppingapi/EcomShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package nelsonssoares.ecomshoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomShoppingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomShoppingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nelsonssoares/ecomshoppingapi/EcomShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nelsonssoares.ecomshoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomShoppingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
