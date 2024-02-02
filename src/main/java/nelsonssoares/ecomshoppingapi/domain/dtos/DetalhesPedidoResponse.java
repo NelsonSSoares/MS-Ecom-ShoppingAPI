5c6a686535f976c27155fde3c28e4aa44ad42d60
Nelson <punkpunkada@gmail.com>
2024-02-02 16:13:07 -0300

    GetOrderByUserId use case need to fix orderdetails, has returned null

--- src/main/java/nelsonssoares/ecomshoppingapi/domain/dtos/DetalhesPedidoResponse.java
+++ src/main/java/nelsonssoares/ecomshoppingapi/domain/dtos/DetalhesPedidoResponse.java
@@ -6,7 +6,6 @@ import nelsonssoares.ecomshoppingapi.outlayers.gateways.clients.entities.Produto
 import java.math.BigDecimal;
 
 @Builder
-//TALVEZ SEJA POSSIVEL EXCLUIR ESTE RECORD
 public record DetalhesPedidoResponse(Produto produto, Integer quantidade, BigDecimal precoTotal) {
 
 }
--- src/main/java/nelsonssoares/ecomshoppingapi/outlayers/entrypoints/PedidoController.java
+++ src/main/java/nelsonssoares/ecomshoppingapi/outlayers/entrypoints/PedidoController.java
@@ -8,11 +8,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nelsonssoares.ecomshoppingapi.domain.dtos.PedidoDTO;
 import nelsonssoares.ecomshoppingapi.domain.dtos.PedidoResponse;
-import nelsonssoares.ecomshoppingapi.domain.entities.Pedido;
 import nelsonssoares.ecomshoppingapi.services.PedidoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static nelsonssoares.ecomshoppingapi.commons.constants.ControllerConstants.*;
 
@@ -39,6 +40,7 @@ public class PedidoController {
         return pedidoService.save(dto);
     }
 
+
     @Operation(summary = "Metodo para buscar pedido por ID", method = "GET")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso!!"),
@@ -51,6 +53,22 @@ public class PedidoController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<PedidoResponse> findOrderById(@PathVariable("id") Integer id) {
         return pedidoService.findOrderById(id);
+    }
+
+
+    @Operation(summary = "Metodo para obter todos os pedidos por ID de usuario", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso!!"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos!"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado!"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválido"),
+            @ApiResponse(responseCode = "500", description = "Erro no Servidor!"),
+    })
+    @GetMapping(USER_ID)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<PedidoResponse>> findOrderByUserId(@PathVariable("id") Integer id) {
+        //DETALHES PEDIDO RETORNANDO NULO
+        return pedidoService.findOrderByUserId(id);
     }
 
 }
