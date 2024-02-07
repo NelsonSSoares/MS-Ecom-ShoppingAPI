d9c6ac013b4f721b94736eb91a4f4ae6178f2591
Nelson <punkpunkada@gmail.com>
2024-02-07 12:26:35 -0300

    bug fix

--- src/main/java/nelsonssoares/ecomshoppingapi/usecases/GetOrderByStatus.java
+++ src/main/java/nelsonssoares/ecomshoppingapi/usecases/GetOrderByStatus.java
@@ -16,13 +16,13 @@ public class GetOrderByStatus {
 
     private final PedidoRepository pedidoRepository;
 
-    public List<PedidoResponse> executeGetOrderByStatus(StatusPedido status){
+    public List<PedidoResponse> executeGetOrderByStatus(StatusPedido statusPedido){
 
-        List<Pedido> pedidos = pedidoRepository.findAllByStatusPedido(status);
+        List<Pedido> pedidos = pedidoRepository.findAllByStatusPedido(statusPedido);
 
         List<Pedido> pedidosAtivos = Constraints.pedidoAtivoList(pedidos);
 
-        System.out.println("usecase:"+pedidosAtivos + " status: "+status + " pedidos: "+pedidos);
+        System.out.println(pedidosAtivos);
 
 
         return null;
